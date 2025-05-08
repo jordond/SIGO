5c2fae0215935eddbcedbe7fb8d8a85832de44b5
Jordon de Hoog <jordon.dehoog@gmail.com>
2025-05-08 15:52:34 -0400

    add lumo components

--- core/app/src/commonMain/kotlin/app/sigot/App.kt
+++ core/app/src/commonMain/kotlin/app/sigot/App.kt
@@ -31,9 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
-import app.sigot.core.resources.IndieFlower_Regular
 import app.sigot.core.resources.Res
 import app.sigot.core.resources.cyclone
 import app.sigot.core.resources.ic_cyclone
@@ -46,7 +44,6 @@ import app.sigot.core.resources.theme
 import app.sigot.core.ui.theme.AppTheme
 import app.sigot.core.ui.theme.LocalThemeIsDark
 import kotlinx.coroutines.isActive
-import org.jetbrains.compose.resources.Font
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -64,7 +61,6 @@ fun App() {
         ) {
             Text(
                 text = stringResource(Res.string.cyclone),
-                fontFamily = FontFamily(Font(Res.font.IndieFlower_Regular)),
                 style = MaterialTheme.typography.displayLarge,
             )
 
