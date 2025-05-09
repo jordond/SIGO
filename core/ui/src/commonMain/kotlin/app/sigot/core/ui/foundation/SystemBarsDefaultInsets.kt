a4750c52645c8e12344dbfdce989f15015501ef2
Jordon de Hoog <jordon.dehoog@gmail.com>
2025-05-09 10:15:42 -0400

    add brutalist styles

---
+++ core/ui/src/commonMain/kotlin/app/sigot/core/ui/foundation/SystemBarsDefaultInsets.kt
@@ -0,0 +1,10 @@
+
+package app.sigot.core.ui.foundation
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.runtime.Composable
+
+public val WindowInsets.Companion.systemBarsForVisualComponents: WindowInsets
+    @Composable
+    get() = systemBars
