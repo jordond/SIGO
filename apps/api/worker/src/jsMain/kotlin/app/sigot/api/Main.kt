bf1ec7c27189b8de552905243ba4de0df52600e6
Jordon de Hoog <jordon.dehoog@gmail.com>
2025-05-29 17:52:07 -0400

    add some DI to apiworker

--- apps/api/worker/src/jsMain/kotlin/app/sigot/api/Main.kt
+++ apps/api/worker/src/jsMain/kotlin/app/sigot/api/Main.kt
@@ -1,17 +1,26 @@
 package app.sigot.api
 
+import app.sigot.api.di.initKoin
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.promise
 import org.w3c.fetch.Request
 import org.w3c.fetch.Response
 import org.w3c.fetch.ResponseInit
+import kotlin.js.Promise
+
+private val koin = initKoin().koin
+private val scope = koin.get<CoroutineScope>()
 
 @OptIn(ExperimentalJsExport::class)
 @JsExport
-fun fetch(request: Request): Response {
-    val headers: dynamic = object {}
-    headers["content-type"] = "text/plain"
+fun fetch(request: Request): Promise<Response> =
+    scope.promise {
+        val headers: dynamic = object {}
+        headers["content-type"] = "text/plain"
 
-    return Response(
-        "Kotlin Worker FOO BAR: ${request.url}",
-        ResponseInit(headers = headers),
-    )
-}
+        val injected = koin.get<String>()
+        Response(
+            "Kotlin Worker: ${request.url}, injected: $injected",
+            ResponseInit(headers = headers),
+        )
+    }
