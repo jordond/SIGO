feedd181dec67bb794096eda73ce43ce2d2d38d3
Jordon de Hoog <jordon.dehoog@gmail.com>
2025-05-30 16:42:03 -0400

    add a reset internal settings button

--- core/model/src/commonMain/kotlin/app/sigot/core/model/settings/InternalSettings.kt
+++ core/model/src/commonMain/kotlin/app/sigot/core/model/settings/InternalSettings.kt
@@ -3,11 +3,13 @@ package app.sigot.core.model.settings
 import app.sigot.build.BuildKonfig
 
 public data class InternalSettings(
-    val enabled: Boolean = false,
+    val enabled: Boolean = BuildKonfig.ENABLE_INTERNAL_SETTINGS,
     val simulateFailure: Boolean = false,
     val backendApiUrl: String = DefaultBackendApiUrl,
     val apiKey: String? = BuildKonfig.FORECAST_API_KEY.takeIf { it.isNotBlank() },
-    val useDirectApi: Boolean = apiKey != null,
+    // This is configured via app-env.properties
+    @Suppress("SimplifyBooleanWithConstants")
+    val useDirectApi: Boolean = BuildKonfig.USE_DIRECT_API || apiKey != null,
 ) {
     public companion object {
         public const val DefaultBackendApiUrl: String = BuildKonfig.BACKEND_URL
