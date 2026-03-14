28cde741cd6907077d7da0ae0ba46f60ef8c3d9a
Jordon de Hoog <jordon.dehoog@gmail.com>
2026-03-14 10:40:02 -0400

    extract ttl constant

--- core/api/server/src/jsMain/kotlin/app/sigot/core/api/server/cache/ForecastCacheProvider.kt
+++ core/api/server/src/jsMain/kotlin/app/sigot/core/api/server/cache/ForecastCacheProvider.kt
@@ -1,5 +1,10 @@
 package app.sigot.core.api.server.cache
 
+import kotlin.time.Duration.Companion.seconds
+
+public const val FORECAST_CACHE_TTL_SECONDS: Int = 900
+public val FORECAST_CACHE_TTL_DURATION: kotlin.time.Duration = FORECAST_CACHE_TTL_SECONDS.seconds
+
 /**
  * Provides access to the KV forecast cache.
  * The cache instance is set once from the Cloudflare env binding on the first request
